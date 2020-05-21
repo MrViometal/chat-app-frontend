5530edcafc4cd49e8f1f2ba2ab1bb8a86d0804bd
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-05-22 01:39:42 +0200

    adding useeffect and setting name and room from location.search

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-function Chat() {
+let socket;
+
+const Chat = (props) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  const { location } = props;
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+
+    return () => {
+      console.log('hi');
+    };
+  }, [ENDPOINT, location.search]);
   return <div>Chat</div>;
-}
+};
 
 export default Chat;
